6bb76587a462acc76ed06d8e2a25ca7c724e0786
Maciej Dowgird <dowgird.maciej@gmail.com>
2020-01-12 16:50:22 +0100

    Initial structure of flask application created

---
+++ flask_app/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+    # app factory
+    app = Flask(__name__, instance_relative_config=None)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
