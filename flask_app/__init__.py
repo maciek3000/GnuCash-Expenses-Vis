16035f75bc4a47c69c993eb9669190cfd07d930a
Maciej Dowgird <dowgird.maciej@gmail.com>
2020-01-13 21:13:45 +0100

    Working version of bkapp added

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -20,13 +20,16 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from .bkapp.bkapp import BokehApp, bkworker
-    #bkapp_instance = BokehApp(os.path.join(app.root_path, 'gnucash', 'gnucash_files', 'finanse_sql.gnucash'))
+    from .bkapp.bkapp import BokehApp
+
+    # test file and names, later on it will be provided by the user
+    bk_file_path_db = os.path.join(app.root_path, 'gnucash', 'gnucash_files', 'finanse_sql.gnucash')
+    bk_port = 9090
+    bk_names = ['Maciek', 'Justyna']
+    bkapp = BokehApp(bk_file_path_db, bk_port, bk_names)
 
     from threading import Thread
-    Thread(target=bkworker).start()
-
-
+    Thread(target=bkapp.bkworker).start()
 
     @app.route('/')
     def index():
@@ -34,8 +37,3 @@ def create_app(test_config=None):
         return render_template('index.html', script=script)
 
     return app
-
-if __name__ == '__main__':
-    pass
-    #app = create_app()
-    #app.run()
--- flask_app/bkapp/bkapp.py
+++ flask_app/bkapp/bkapp.py
@@ -1,52 +1,30 @@
-from bokeh.models import ColumnDataSource, Slider
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.server.server import Server
 from bokeh.themes import Theme
-from bokeh.layouts import column
 from ..gnucash.gnucash_db_parser import GnuCashDBParser
 from tornado.ioloop import IOLoop
 import os
 
 class BokehApp(object):
 
-    def __init__(self, file_path):
-        self.datasource = GnuCashDBParser(file_path, names=['Maciek', 'Justyna']).get_df()
+    def __init__(self, file_path, port, names):
+        self.datasource = GnuCashDBParser(file_path, names=names).get_df()
+        self.port = port
 
     def provide_viz(self, doc):
-        agg = self.datasource.groupby(['MonthYear']).sum().reset_index().sort_values()
+
+        agg = self.datasource.groupby(['MonthYear']).sum().reset_index().sort_values(by='MonthYear')
         source = ColumnDataSource(agg)
 
         p = figure(width=480, height=480, x_range=agg['MonthYear'])
         p.vbar(x='MonthYear', width=0.9, top='Price', source=source)
 
-def provide_viz(doc):
+        doc.add_root(p)
+        doc.theme = Theme(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), "theme.yaml"))
 
-    from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
-
-    df = sea_surface_temperature.copy()
-    source = ColumnDataSource(data=df)
-
-    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
-                  title="Sea Surface Temperature at 43.18, -70.43")
-    plot.line('time', 'temperature', source=source)
-
-    def callback(attr, old, new):
-        if new == 0:
-            data = df
-        else:
-            data = df.rolling('{0}D'.format(new)).mean()
-        source.data = ColumnDataSource(data=data).data
-
-    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
-    slider.on_change('value', callback)
-
-    doc.add_root(column(slider, plot))
-
-    doc.theme = Theme(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), "theme.yaml"))
-
-def bkworker():
-    server = Server({'/provide': provide_viz}, io_loop=IOLoop(), allow_websocket_origin=['127.0.0.1:5000', '127.0.0.1:9090'],
-                    port=9090)
-    server.start()
-    server.io_loop.start()
-
+    def bkworker(self):
+        server = Server({'/provide': self.provide_viz}, io_loop=IOLoop(), allow_websocket_origin=['127.0.0.1:5000'],
+                        port=self.port)
+        server.start()
+        server.io_loop.start()
