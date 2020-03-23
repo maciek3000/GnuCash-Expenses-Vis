ff80d30073a83f39f1cce802dc50189098c76dac
maciek3000 <42045514+maciek3000@users.noreply.github.com>
2020-03-23 18:26:56 +0100

    quick commit with beginning of modifications to category filtering in bk_category.py

--- flask_app/bkapp/bk_category.py
+++ flask_app/bkapp/bk_category.py
@@ -105,14 +105,19 @@ class Category(object):
         self.shop = shop_colname
 
         self.original_df = None  # original dataframe passed to the gridplot function
-        self.selected_months_df = None  # dataframe that is filtered via BoxSelectTool
-        self.grouped_categories_df = None  # dataframe grouped by month and category
-        self.selected_months_grouped_categories_df = None  # dataframe grouped by month and category and filtered to
+        self.current_category_df = None
+        self.selected_months_and_category_df = None
+
+        # self.selected_months_df = None  # dataframe that is filtered via BoxSelectTool
+
+        # self.grouped_categories_df = None  # dataframe grouped by month and category
+        # self.selected_months_grouped_categories_df = None  # dataframe grouped by month and category and filtered to
         # selected months
 
         self.categories = None
         self.months = None
         self.chosen_category = None
+        self.chosen_months = None
 
         self.g_category_title = "Category Title"
         self.g_dropdown = "Dropdown"
@@ -131,18 +136,24 @@ class Category(object):
     def gridplot(self, dataframe):
 
         self.original_df = dataframe
-        self.selected_months_df = dataframe
+        self.current_category_df = dataframe
+        self.selected_months_and_category_df = dataframe
+
+        # TODO: categories will be extracted depending on settings
         self.categories = unique_values_from_column(dataframe, self.category)
         self.months = unique_values_from_column(dataframe, self.monthyear)
-        self.grouped_categories_df = self.__create_grouped_categories_df(dataframe)
 
-        initial_category = self.categories[0]
+
+        # self.grouped_categories_df = self.__create_grouped_categories_df(dataframe)
+
+        self.chosen_category = self.categories[0]
         self.grid_elem_dict, self.grid_source_dict = self.initialize_grid_elements(initial_category)
         self.update_grid_on_category_change(initial_category)
 
         def dropdown_callback(attr, old, new):
             if new != old:
-                self.update_grid_on_category_change(new)
+                self.chosen_category = new
+                self.update_grid_on_category_change()
 
         self.grid_elem_dict[self.g_dropdown].on_change("value", dropdown_callback)
 
@@ -202,25 +213,25 @@ class Category(object):
         source_dict[self.g_transactions] = self.__create_transactions_source()
         elem_dict[self.g_transactions] = self.__create_transactions_table(source_dict[self.g_transactions])
 
-        elem_dict[self.g_dropdown] = Select(value=initial_category, options=self.categories,
+        elem_dict[self.g_dropdown] = Select(value=self.chosen_category, options=self.categories,
                                             css_classes=["category_dropdown"])
 
         return elem_dict, source_dict
 
-    def update_grid_on_category_change(self, category):
+    def update_grid_on_category_change(self):
 
-        self.chosen_category = category
+        self.__update_dataframes()
 
-        self.__update_category_title(category)
-        self.__update_statistics_table(category)
-        self.__update_total_from_category(category)
-        self.__update_category_fraction(category)
-        self.__update_total_products_from_category(category)
-        self.__update_category_products_fraction(category)
+        self.__update_category_title()
+        self.__update_statistics_table()
+        self.__update_total_from_category()
+        self.__update_category_fraction()
+        self.__update_total_products_from_category()
+        self.__update_category_products_fraction()
 
-        self.__update_line_plot(category)
-        self.__update_product_histogram_table(category)
-        self.__update_transactions_table(category)
+        self.__update_line_plot()
+        self.__update_product_histogram_table()
+        self.__update_transactions_table()
 
     def update_grid_on_selection_change(self, new_indices):
 
@@ -407,5 +418,5 @@ class Category(object):
         return values
 
     def __create_grouped_categories_df(self, dataframe):
-        df = aggregated_dataframe_sum(dataframe, [self.monthyear, self.category])
+        df = aggregated_dataframe_sum(dataframe, [self.monthyear])
         return df
